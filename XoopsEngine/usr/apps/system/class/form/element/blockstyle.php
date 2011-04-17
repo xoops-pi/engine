cd0b2d0b18c57b0da9a42fad52edee4a839396f3
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2011-04-17 18:15:10 +0800

    massive updates on block and theme

---
+++ XoopsEngine/usr/apps/system/class/form/element/blockstyle.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Block style selection
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright       Xoops Engine http://www.xoopsengine.org
+ * @license         http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @author          Taiwen Jiang <phppp@users.sourceforge.net>
+ * @since           3.0
+ * @category        Xoops_Module
+ * @package         System
+ * @version         $Id$
+ */
+
+class App_System_Form_Element_Blockstyle extends Zend_Form_Element_Select
+{
+    /**
+     * Constructor
+     *
+     * @param  string|array|Zend_Config $spec Element name or configuration
+     * @param  string|array|Zend_Config $options Element value or configuration
+     * @return void
+     */
+    public function __construct($spec, $options = null)
+    {
+        parent::__construct($spec, $options);
+        $this->setServiceOptions();
+    }
+
+    protected function setServiceOptions()
+    {
+        $list = array(
+            ''          => 'Default',
+            'xoops-block-style-custom'    => 'Custom',
+        );
+
+        $this->setMultiOptions($list);
+    }
+}
