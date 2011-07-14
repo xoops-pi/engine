5a52aad8e1f34f72516f24038cf0d16880322bb1
Taiwen Jiang <taiwenjiang@tsinghua.org.cn>
2011-07-14 18:15:06 +0800

    Switched installer directory to "setup"

--- XoopsEngine/htdocs/boot.php
+++ XoopsEngine/htdocs/boot.php
@@ -16,5 +16,5 @@
  */
 
 /* Redirect to installer */
-header("Location: http://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), DIRECTORY_SEPARATOR) . "/install");
+header("Location: http://" . $_SERVER['HTTP_HOST'] . rtrim(dirname($_SERVER['PHP_SELF']), DIRECTORY_SEPARATOR) . "/setup");
 exit;
